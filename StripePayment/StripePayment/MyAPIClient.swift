26f74301e302d4d79a886fa3da386dd17f41a55f
brightsummit.co <naseemoyebola@gmail.com>
2022-01-20 11:22:58 +0100

    initial commit

---
+++ StripePayment/StripePayment/MyAPIClient.swift
@@ -0,0 +1,33 @@
+//
+//  MyAPIClient.swift
+//  StripePayment
+//
+//  Created by naseem on 20/01/2022.
+//
+
+import Foundation
+import Stripe
+import Alamofire
+
+class MyAPIClient: NSObject, STPCustomerEphemeralKeyProvider {
+//    func createCustomerKey(withAPIVersion apiVersion: String, completion: @escaping STPJSONResponseCompletionBlock) {
+//
+//    }
+    
+    class func createcustomer() {
+        var customerDetailParams = [String:String]()
+        customerDetailParams["email"] = "nas@gmail.com"
+        customerDetailParams["phone"] = "235465"
+        customerDetailParams["name"] = "testing"
+        
+        AF.request(URL(string: "http//192.168.88.74.:8080/StripeBackend/createcustomer.php")!, method: .post, parameters: customerDetailParams, encoding: URLEncoding.default, headers: nil).responseJSON { (response) in
+            if response.result.isSuccess {
+                debugPrint(response.data)
+            } else {
+                debugPrint(response.error)
+                debugPrint(response.debugDescription)
+            }
+        }
+    }
+}
+
---
+++ SwiftUIStripePayment/SwiftUIStripePayment/SwiftUIStripePaymentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStripePaymentApp.swift
+//  SwiftUIStripePayment
+//
+//  Created by naseem on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStripePaymentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
